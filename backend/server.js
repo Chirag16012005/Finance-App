a6ce4fa7b1728a62ba651039a5363e4b98c4e843
Chirag16012005 <202301259@daiict.ac.in>
2026-04-06 21:43:36 +0530

    start script

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-
+const path=require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const { MONGO_URI, PORT } = require("./config/env");
 
+app.use(express.static(path.join(__dirname, "public")));
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log("DB connected");
