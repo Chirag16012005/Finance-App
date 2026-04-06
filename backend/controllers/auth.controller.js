b8fd89a9e4f8e9b83b89781c38f10091feb3d793
Chirag16012005 <202301259@daiict.ac.in>
2026-04-06 21:11:42 +0530

    README changed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -102,4 +102,14 @@ const LoginController = async (req, res) => {
   }
 };
 
-module.exports = { RegisterController, LoginController };
+const getMeController = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { RegisterController, LoginController, getMeController };
